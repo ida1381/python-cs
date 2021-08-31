9fc17f1772b47204debfc07e8afb6123874ec8d1
ida1381 <89854419+ida1381@users.noreply.github.com>
2021-08-31 18:41:14 +0430

    Add files via upload

---
+++ dictunary.py
@@ -0,0 +1,18 @@
+dictunary1={"hello" : "salam" , "age" : "sen", "name" : "esm" , "number" : "shomare" }
+a=input("dictunary")
+while a!="0":
+    
+    if a=="hello":
+        #a1= input("ask me to translate you :")
+        print( dictunary1["hello"])
+    elif a=="age":
+        #b=input("ask me to tranlate you")
+        print( dictunary1["age"])
+    elif a=="name":
+        #c=input("ask me to translate you")
+        print( dictunary1["name"])
+    elif a=="number":
+        #d=input("ask me to translate you:")
+        print(dictunary1["number"])
+    a=input("dictunary")
+print("corect")
